80bf350fc6c2201c9d6c52551beb1d17a57d711e
Anastasia849 <nst10knv@gmail.com>
2020-10-19 18:56:06 +0300

    add 16prac

---
+++ Practice16/src/ru/mirea/practice16/Order.java
@@ -0,0 +1,20 @@
+package ru.mirea.practice16;
+
+public interface Order {
+    boolean add(Item item);
+
+    boolean delete(String name);
+
+    int deleteAll(String name);
+
+    int numOfOrders();
+
+    Object[] getArray();
+
+    double getSumPrice();
+
+    Object[] getArrayOfNames();
+
+    Object[] getSortedArray();
+}
+
